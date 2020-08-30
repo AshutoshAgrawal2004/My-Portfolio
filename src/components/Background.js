fe44f0b2fae5d78feb7eeeaf140acd10783073ad
Ashutosh Agrawal <agrawalnidhi635@gmail.com>
2020-08-30 12:04:25 +0530

    About Page perfected

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Sketch from 'react-p5';
-import Bg from './bg.jpg';
 
-const brushSize = 5;
-
+import BGL from './images/Steve_landscape.jpg';
+import BGP from './images/Steve Portrait.jpg';
 var img;
-var particles = [];
 var window = { width: 1980, height: 1080 };
-const Background = ({}) => {
+const Background = ({ isPortrait }) => {
+	let particles = [];
+
+	let initNoOfParticles = isPortrait ? 100 : 300;
+	let brushSize = isPortrait ? 15 : 21;
+
 	return (
 		<Sketch
 			preload={(p5) => {
 				// img = p5.loadImage(Bg);
-				img = p5.loadImage(
-					'https://photos5.appleinsider.com/gallery/product_pages/189-hero.jpg'
-				);
+				img = isPortrait ? p5.loadImage(BGP) : p5.loadImage(BGL);
 			}}
 			setup={(p5, parent) => {
 				console.log(p5);
@@ -24,21 +25,26 @@ const Background = ({}) => {
 				p5.pixelDensity(1);
 				// img = loadImage("Pikachu Jump.png");
 				p5.background(51);
+				p5.frameRate(30);
 			}}
 			draw={(p5) => {
 				// requestAnimationFrame(p5.draw);
 				// background(255);
 				// image(img, 400, 200);
-				for (let i = 0; i < 25; i++) {
+				if (brushSize > 3 && p5.frameCount % 50 == 0) {
+					brushSize -= 3;
+				}
+				for (let i = 0; i < initNoOfParticles / brushSize; i++) {
 					particles.push(
 						new Particle(
 							p5.random(p5.width),
 							p5.random(p5.height),
-							p5
+							p5,
+							brushSize
 						)
 					);
 				}
-				if (p5.frameCount % 500 == 0) {
+				if (p5.frameCount % 50 == 0) {
 					particles.splice(0, particles.length);
 				}
 				for (var j = 0; j < particles.length; j++) {
@@ -51,7 +57,7 @@ const Background = ({}) => {
 	);
 };
 class Particle {
-	constructor(x, y, p5) {
+	constructor(x, y, p5, bs) {
 		this.x = x;
 		this.y = y;
 		this.xvel = 10;
@@ -59,18 +65,14 @@ class Particle {
 		this.lastx = this.x;
 		this.lasty = this.y;
 		this.p5 = p5;
+		this.bs = bs;
 	}
 	update() {
 		this.lastx = this.x;
 		this.lasty = this.y;
 		this.y += Math.floor(this.p5.random(0, this.yvel));
 		this.x += Math.floor(this.p5.random(0, this.xvel));
-		// this.y += Math.floor(this.p5.random(-10, 10));
-		// this.x += brushSize;
-		// if(this.x > this.p5.width) {
-		//   this.y += brushSize;
-		//   this.x = -this.x;
-		// }
+
 		if (this.y > this.p5.height || this.y < 0) {
 			this.yvel *= -1;
 		}
@@ -81,71 +83,24 @@ class Particle {
 		this.y = this.p5.constrain(this.y, 0, this.p5.height);
 	}
 	show() {
-		// this.p5.noStroke();
 		var px = this.p5.map(this.x, 0, this.p5.width, 0, img.width);
 		var py = this.p5.map(this.y, 0, this.p5.height, 0, img.height);
 		var col = img.get(px, py);
-		// this.p5.strokeWeight(this.p5.random(brushSize));
-		//this.p5.stroke(col[0], col[1], col[2]);
 		this.p5.noStroke();
-		//stroke(0);
-		//this.p5.stroke(this.p5.random(255), this.p5.random(255), this.p5.random(255));
-		//this.p5.strokeWeight(this.p5.random(3));
 		this.p5.fill(col[0], col[1], col[2]);
-		// line(this.x, this.y, this.lastx, this.lasty);
 		this.p5.beginShape();
 		for (
 			let a = 0;
 			a <= 360;
 			a += 360 / this.p5.floor(this.p5.random(3, 6))
 		) {
-			let xx = brushSize * this.p5.sin(a) + this.x;
-			let yy = brushSize * this.p5.cos(a) + this.y;
+			let xx = this.bs * this.p5.sin(a) + this.x;
+			let yy = this.bs * this.p5.cos(a) + this.y;
 			this.p5.vertex(xx, yy);
 		}
 		this.p5.endShape(this.p5.CLOSE);
-		// ellipse(this.x, this.y, this.p5.random(brushSize), this.p5.random(brushSize));
+		// this.p5.ellipse(this.x, this.y, this.p5.random(this.bs), this.p5.random(this.bs));
 	}
 }
 
 export default Background;
-/*
-function preload() {
-	// var selector = document.getElementById('chooseimg');
-	// var imgsrc = $('#img');
-	// selector.addEventListener('change', function() {
-	//   imgsrc.attr('src', selector.value);
-	//   console.log(selector.value);
-	// });
-	// img = loadImage(imgsrc.attr('src'));
-	img = loadImage(
-		'https://cors-anywhere.herokuapp.com/' + getImage(profileLink)
-	);
-}
-
-function setup() {
-	// var mycan = createCanvas(window.innerWidth, window.innerHeight);
-	// canvas = document.getElementById("canvas");
-	var mycan = createCanvas(img.width, img.height);
-	mycan.parent('canvascontainer');
-	pixelDensity(1);
-	// img = loadImage("Pikachu Jump.png");
-	background(255);
-	particles = [];
-}
-
-function draw() {
-	// background(255);
-	// image(img, 400, 200);
-	particles.push(new Particle(p5.random(width), p5.random(height)));
-	if (frameCount % 1000 == 0) {
-		particles.splice(0, particles.length);
-	}
-	for (var j = 0; j < particles.length; j++) {
-		particles[j].show();
-		particles[j].update();
-	}
-	// image.loadPixels();
-}
-
-*/
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { MDBContainer, MDBView, MDBMask } from 'mdbreact';
 import Background from './Background';
+import Typewriter from 'typewriter-effect';
+import { useMediaQuery } from 'react-responsive';
 
 const Home = () => {
+	const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+
 	return (
 		<MDBContainer className='m-0 p-0' style={{ backgroundColor: '#333' }}>
 			<MDBView>
-				<Background />
+				<Background isPortrait={isPortrait} />
 
 				<MDBMask overlay='stylish-strong'>
 					<div
@@ -14,31 +18,51 @@ const Home = () => {
 						style={{
 							position: 'absolute',
 							color: 'white',
-							top: '50%',
+							top: '40%',
 							left: '0',
 							right: '0',
 							textAlign: 'center',
 							// textShadow: '2px 2px 5px black',
 						}}
 					>
-						<h3 style={styles.heading}>
-							Hello, My name is Ashutosh Agrawal
-						</h3>
-						<h4 style={styles.heading}>
-							I am a Full Stack Web Developer, Creative Coder and
-							MERN Specialist.
+						<h3 style={{ fontWeight: '900' }}>Ashutosh Agrawal</h3>
+						<h4>
+							I am
+							<Typewriter
+								options={{
+									strings: [
+										'The Crazy One',
+										'Full Stack Web Developer',
+										'MERN Specialist',
+										'Creative Coder',
+									],
+									autoStart: true,
+									loop: true,
+								}}
+							/>
+							{/* <Typewriter
+								onInit={(typewriter) => {
+									typewriter
+										.changeDeleteSpeed(10)
+										.typeString('I am a ')
+										.typeString('Full Stack Web Developer')
+										.pauseFor(100)
+										.deleteChars(24)
+										.typeString('MERN Specialist')
+										.pauseFor(100)
+										.deleteChars(15)
+										.typeString('Creative Coder')
+										.pauseFor(100)
+										.deleteChars(14)
+										.start();
+								}}
+							/> */}
 						</h4>
 					</div>
 				</MDBMask>
 			</MDBView>
 		</MDBContainer>
 	);
-};
-
-const styles = {
-	heading: {
-		fontWeight: '900',
-	},
 };
 
 export default Home;
