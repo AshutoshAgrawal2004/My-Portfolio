94039ea9ead451020b8e9a47e634b014f10e5ad2
Ashutosh Agrawal <agrawalnidhi635@gmail.com>
2020-08-31 23:32:43 +0530

    Updates

---
+++ precache-manifest.99911648710ffd5c4fd1f963e1491d53.js
@@ -0,0 +1,358 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea17e8d07b776b2471528bff1febe19a",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/index.html"
+  },
+  {
+    "revision": "53f418a4b0f6d4c8bd9e",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/css/2.3957d38e.chunk.css"
+  },
+  {
+    "revision": "a8b185cb7654099701db",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/css/main.0052447b.chunk.css"
+  },
+  {
+    "revision": "53f418a4b0f6d4c8bd9e",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/js/2.2acc36da.chunk.js"
+  },
+  {
+    "revision": "c6114de2aeece33731d8bfc9c9813503",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/js/2.2acc36da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b185cb7654099701db",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/js/main.27862ed5.chunk.js"
+  },
+  {
+    "revision": "0c30e515f3b4f2076179",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/js/runtime-main.c0380969.js"
+  },
+  {
+    "revision": "ddd1816fc50258db94dbe3e5a01c79fe",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Applicant1.ddd1816f.png"
+  },
+  {
+    "revision": "823db13808f7770286b8d7afbb29d3de",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Applicant2.823db138.png"
+  },
+  {
+    "revision": "c9833e25bf802803721899f3bcc87903",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/BaseApparel1.c9833e25.png"
+  },
+  {
+    "revision": "80404e22ce4a7d837d2faa739cc54621",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Blog1.80404e22.png"
+  },
+  {
+    "revision": "db5f9d8ba4605bbed9a3fb88fdc9f625",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Blog2.db5f9d8b.png"
+  },
+  {
+    "revision": "0a3da6e3aca5fde55a3e3910d484ff46",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Blog3.0a3da6e3.png"
+  },
+  {
+    "revision": "6f9318eb06b52ecc2efac952ed0fc59c",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Blog4.6f9318eb.png"
+  },
+  {
+    "revision": "5209029d9446783a2472fa38a59cdcc8",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Bootstrap.5209029d.png"
+  },
+  {
+    "revision": "acd90b1f96e58610e064ce6e0d006411",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/C.acd90b1f.png"
+  },
+  {
+    "revision": "497581c1a6b99b28d17b04d017043c85",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/CP1.497581c1.png"
+  },
+  {
+    "revision": "929764101a169da34aea32a3990f1f82",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/CP2.92976410.png"
+  },
+  {
+    "revision": "ee0f06cda5c7d458ac99cfdef5914e71",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/CP3.ee0f06cd.png"
+  },
+  {
+    "revision": "0a84260779d43dc98f6d1532c17d0e17",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/CSS.0a842607.png"
+  },
+  {
+    "revision": "cfee73c9e5da81457ca296fbd63c4fc4",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/CSSForm1.cfee73c9.png"
+  },
+  {
+    "revision": "b4d867ffb1ac9ed1e397923ea0176ab8",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Chrome.b4d867ff.png"
+  },
+  {
+    "revision": "afdc89faf4aa6fc1af19f43fe561f12a",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Cpp.afdc89fa.png"
+  },
+  {
+    "revision": "ca90dccaf2ba3f8435def5005246cda8",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/ELearning1.ca90dcca.png"
+  },
+  {
+    "revision": "1964255510aa0110fa8c4eca1963c70b",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/ELearning2.19642555.png"
+  },
+  {
+    "revision": "5527b08d073b40fe36b9a04b85285f87",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/ELearning3.5527b08d.png"
+  },
+  {
+    "revision": "bd06080fb9c5e7d38e07448075beb949",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/ELearning4.bd06080f.png"
+  },
+  {
+    "revision": "0cc6fbcb49cd9ce2b97e9f061db1eab6",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/ELearning5.0cc6fbcb.png"
+  },
+  {
+    "revision": "b62fd8079b2613ac5d65ff9341d5def2",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/ExpressJS.b62fd807.png"
+  },
+  {
+    "revision": "b151a8e9091bd70f434c599844e8fa9a",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/FacebookAds.b151a8e9.png"
+  },
+  {
+    "revision": "0271d2c6e35916cc9399994986eb5a64",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Firebase.0271d2c6.png"
+  },
+  {
+    "revision": "8ef99f9d63f30fa7e53c30b8e9177c91",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/GoogleAnalytics.8ef99f9d.png"
+  },
+  {
+    "revision": "ac3f9df150b22bce4333d23839450157",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Heroku.ac3f9df1.png"
+  },
+  {
+    "revision": "ddedc1040df207ede2bbc3dcfe9b22c1",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Html.ddedc104.png"
+  },
+  {
+    "revision": "c07362393223738cc58c41edb1db3f6b",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/JQuery.c0736239.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/JS.0cded3a3.png"
+  },
+  {
+    "revision": "8118755972b20371f5c71a73d653c212",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/JWT.81187559.png"
+  },
+  {
+    "revision": "5bb7a13079400b7cc9d0db2adc440607",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Materialize.5bb7a130.png"
+  },
+  {
+    "revision": "bcf80ceafe56e3c71bc17e2bf4b9a554",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Maths1.bcf80cea.png"
+  },
+  {
+    "revision": "d37688f45bc7eb9162bb19e9d9f23cc5",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Maths2.d37688f4.png"
+  },
+  {
+    "revision": "84e663362102fd399ed0118f21a43559",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Maths3.84e66336.png"
+  },
+  {
+    "revision": "86efd68cb0f9e944c8a4a353ce9e04ab",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Maytree1.86efd68c.png"
+  },
+  {
+    "revision": "997616f932381b492d9f753b942fbb82",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Maytree2.997616f9.png"
+  },
+  {
+    "revision": "7c95adb7ec1a9be6a4b5922115124c7f",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Maytree3.7c95adb7.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/NodeJS.6dcf6999.png"
+  },
+  {
+    "revision": "6f76a09eb29931e0c859e7e1435c5758",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/OAuth.6f76a09e.png"
+  },
+  {
+    "revision": "3a3b6c83c06dfcf8cee12584dddf5f00",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/PeriodicTable1.3a3b6c83.png"
+  },
+  {
+    "revision": "544c47210fad76f809e978fc334d8ecc",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/PeriodicTable2.544c4721.png"
+  },
+  {
+    "revision": "641c1482d0b514e5a992d7b36bbbe09e",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Pokedex1.641c1482.png"
+  },
+  {
+    "revision": "fd07f470a41d5c7fad41806434ff1bb1",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Pokedex2.fd07f470.png"
+  },
+  {
+    "revision": "d48c54207372eb953a386342e8596ce0",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Pokedex3.d48c5420.png"
+  },
+  {
+    "revision": "0362f17a4dbeac7e4cd544c725511dfc",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/ProjectTracking1.0362f17a.png"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Python.b9bbb1d7.png"
+  },
+  {
+    "revision": "af2745706a6a4d285a74cd6de183d2a0",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/ReactJS.af274570.png"
+  },
+  {
+    "revision": "62cc7d7a7701fc5c78e8bfdfe1c58ad7",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Redux.62cc7d7a.png"
+  },
+  {
+    "revision": "81357b4e75738c4031e49b0fa63aa468",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Regex.81357b4e.png"
+  },
+  {
+    "revision": "bd6214957f0c2b935b5c0dbef90dcc15",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/SingleGrid1.bd621495.png"
+  },
+  {
+    "revision": "fb91bb0228df4edd206a7b2c5db59679",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/SnowFlake1.fb91bb02.png"
+  },
+  {
+    "revision": "9baec4f2e9ecd6d18556916b90fc5c5b",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/SnowFlake2.9baec4f2.png"
+  },
+  {
+    "revision": "0b87679ab4a6d4443bc3626afbcd8668",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/SnowFlake3.0b87679a.png"
+  },
+  {
+    "revision": "4e621bc21e54c18ffd3011740b2d317a",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Soccer1.4e621bc2.png"
+  },
+  {
+    "revision": "67ffd2199942e818064c1f38e0f82303",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Soccer2.67ffd219.png"
+  },
+  {
+    "revision": "26ca03a79628af44241860934c3b659e",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Soccer3.26ca03a7.png"
+  },
+  {
+    "revision": "4cb9ecf17526b09e164702939c0018d0",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/SocketIO.4cb9ecf1.png"
+  },
+  {
+    "revision": "7c739ff55c8e79bdcdf4bda90920b7fb",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Steve Portrait.7c739ff5.jpg"
+  },
+  {
+    "revision": "f3f61da72ab63defa7e5f46f24ea70ee",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Steve_landscape.f3f61da7.jpg"
+  },
+  {
+    "revision": "225fbbe09689a4a3219ae1e00fe2280b",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/VSCode.225fbbe0.png"
+  },
+  {
+    "revision": "8956587c89d9e28c3eb847dec0ca7a41",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Workshop1.8956587c.png"
+  },
+  {
+    "revision": "83a011d926097ad9572c5a0b7898610d",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Workshop2.83a011d9.png"
+  },
+  {
+    "revision": "c72f20874ac523885bfcf74f0be09a80",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/Workshop3.c72f2087.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "5c8b342de56e819fa42a0d28fbab8f60",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/p5JS.5c8b342d.png"
+  },
+  {
+    "revision": "8f3bbc4f5c216975632bca4bde4e038c",
+    "url": "/AshutoshAgrawal2004/My-Portfolio/static/media/sololearnIcon.8f3bbc4f.svg"
+  }
+]);
